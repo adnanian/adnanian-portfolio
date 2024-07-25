0e8a2e3d17440e13f74b85045d9c852d6e950f89
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-07-25 13:18:50 -0500

    Creating header, provided simple styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
+import React from "react";
 import Footer from "./navigation/Footer";
 import Header from "./navigation/Header";
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <Header/>
       <main>
 
       </main>
       <Footer/>
-    </>
+    </React.Fragment>
   );
 }
 
--- client/src/navigation/Header.js
+++ client/src/navigation/Header.js
@@ -1,4 +1,61 @@
+import "../styles/Header.css";
+
+/**
+ * https://www.youtube.com/watch?v=aNDqzlAKmZc
+ * 
+ * @returns 
+ */
 export default function Header() {
+
+    const navigationalLinks = [
+        (
+            <a
+                key="about"
+                className="nav-link"
+                href="#about"
+                title="Click here to learn more about Adnan Wazwaz."
+            >
+                About
+            </a>
+        ),
+        (
+            <a
+                className="nav-link"
+                href="#skills"
+                title="Click here to learn more about Adnan's skills."
+            >
+                Skills
+            </a>
+        ),
+        (
+            <a
+                className="nav-link"
+                href="#technical-projects"
+                title="Click here to view Adnan's technical works."
+            >
+                Technical Projects
+            </a>
+        ),
+        (
+            <a
+                className="nav-link"
+                href="#side-projects"
+                title="Click here to view Adnan's projects that don't involve software engineering."
+            >
+                Side Projects
+            </a>
+        ),
+        (
+            <a
+                className="nav-link"
+                href="#contact"
+                title="Click here to navigate to a form where you can get in touch with Adnan."
+            >
+                Contact
+            </a>
+        )
+    ];
+
     return (
         <header>
             <a
@@ -14,42 +71,11 @@ export default function Header() {
                 </figure>
             </a>
             <span>Adnan Wazwaz</span>
-            <nav>
-            <a
-                    className="nav-link"
-                    href="#about"
-                    title="Click here to learn more about Adnan Wazwaz."
-                >
-                    About
-                </a>
-                <a
-                    className="nav-link"
-                    href="#skills"
-                    title="Click here to learn more about Adnan's skills."
-                >
-                    Skills
-                </a>
-                <a
-                    className="nav-link"
-                    href="#technical-projects"
-                    title="Click here to view Adnan's technical works."
-                >
-                    Technical Projects
-                </a>
-                <a
-                    className="nav-link"
-                    href="#side-projects"
-                    title="Click here to view Adnan's projects that don't involve software engineering."
-                >
-                    Side Projects
-                </a>
-                <a
-                    className="nav-link"
-                    href="#contact"
-                    title="Click here to navigate to a form where you can get in touch with Adnan."
-                >
-                    Contact
-                </a>
+            <nav className="off-screen-menu">
+                {navigationalLinks}
+            </nav>
+            <nav className="hamburger-menu">
+
             </nav>
         </header>
     )
