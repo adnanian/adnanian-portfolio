0def2fa0abd5960909b858e671f7edfa27d7aeb8
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-07-28 11:04:45 -0500

    Create and style simple home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Footer from "./navigation/Footer";
 import Header from "./navigation/Header";
+import Home from "./sections/Home";
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
       <main>
+        <Home/>
       </main>
       <Footer/>
     </React.Fragment>
---
+++ client/src/sections/Home.js
@@ -0,0 +1,17 @@
+import "../styles/Home.css";
+
+export default function Home() {
+    return (
+        <div id="home">
+            <br/>
+            <br/>
+            <h1>Hi, I'm Adnan Wazwaz</h1>
+            <h3>I'm a Full Stack Software Engineer, Card Game Creator, and Arabic Enthusiast</h3>
+            <br/>
+            <p>Welcome to the Adnanian Order. Here, you will find all my greatest ideas.
+                Scroll down or click on the navigational links above to learn more.
+            </p>
+            <span className="down-arrow">&#8681;</span>
+        </div>
+    );
+}
