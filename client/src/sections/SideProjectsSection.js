28fbdbcb479373e1408fef6e9dcba6dd2875df9b
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-07-30 06:02:15 -0500

    Start side projects section

--- client/src/sections/SideProjectsSection.js
+++ client/src/sections/SideProjectsSection.js
@@ -1,7 +1,20 @@
+import "../styles/SideProjectsSection.css";
+
 export default function SideProjectsSection() {
     return (
         <section id="side-projects">
             <h1>My Side Projects</h1>
+            <p>At this time, the only public active side-project is Mada, so for
+                now, I will dedicate this entire section to Mada.</p>
+            <br/>
+            <p>Mada is a Halal, math-based strategy card game for 2-8 players.
+                The premise of the game is that each player has a personal
+                number, called a position, which must stay within a universal
+                range. You win by eliminating other players' positions from the
+                range, until you're the last active player. If you would like to
+                learn how to play, you can contact me using the form in the next
+                section for an inquiry.
+            </p>
         </section>
     );
 }
